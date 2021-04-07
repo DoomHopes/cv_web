66ef96d61eeb53835a162bbb984e2e556ddc3b2f
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-04-07 12:05:09 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bender'),
+      ),
+      body: Center(
+        child: Container(
+          height: 500,
+          width: 500,
+          child: Column(
+            children: <Widget>[
+              Image.asset('assets/Bender.jpg'),
+              Text(
+                'Bite my shiny metal ass',
+                style: TextStyle(fontSize: 25),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
