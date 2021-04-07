3cc476d1495377e274adcb2e8963e1079c0ca64e
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-04-07 14:18:11 +0300

    theme changer

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -17,8 +17,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "6937f1c36ff8cfab06477f9e183b446a",
-"/": "6937f1c36ff8cfab06477f9e183b446a",
+"index.html": "528d9fa0c3ff70d1b45fae35a5f828e9",
+"/": "528d9fa0c3ff70d1b45fae35a5f828e9",
 "main.dart.js": "db01dc04266458b4e1c5c654e6bdda05",
 "manifest.json": "5c27a8fc17f35d43997f02cb5948c754",
 "version.json": "b611757f7ec8b121dd2e66bc658e8703"
