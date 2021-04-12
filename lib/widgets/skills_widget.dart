2c72f413f5c8a1d1c8b35a587df9c15d2b7c93d6
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-04-12 12:14:01 +0300

    change skill widget

--- lib/widgets/skills_widget.dart
+++ lib/widgets/skills_widget.dart
@@ -5,65 +5,56 @@ import 'package:flutter/cupertino.dart';
 class SkillsWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              ' - Flutter;',
-              style: kTextStyle,
-            ),
-            Text(
-              ' - Dart;',
-              style: kTextStyle,
-            ),
-            Text(
-              ' - Provider + ChangeNotifier;',
-              style: kTextStyle,
-            ),
-            Text(
-              ' - ' + S.of(context).ClientServer + ' (with REST API);',
-              style: kTextStyle,
-            ),
-            Text(
-              ' - Java (Android Studio);',
-              style: kTextStyle,
-            ),
-            Text(
-              ' - SQL;',
-              style: kTextStyle,
-            ),
-            Text(
-              ' - NoSQL (MongoDB, Hive);',
-              style: kTextStyle,
-            ),
-          ],
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          ' - Flutter;',
+          style: kTextStyle,
         ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              ' - MVP, MVC, MVVM;',
-              style: kTextStyle,
-            ),
-            Text(
-              ' - ' + S.of(context).EnglishLevel,
-              style: kTextStyle,
-            ),
-            Text(
-              ' - SOLID;',
-              style: kTextStyle,
-            ),
-            Text(
-              ' - YAGNI;',
-              style: kTextStyle,
-            ),
-            Text(
-              ' - DRY;',
-              style: kTextStyle,
-            ),
-          ],
+        Text(
+          ' - Dart;',
+          style: kTextStyle,
+        ),
+        Text(
+          ' - Provider + ChangeNotifier;',
+          style: kTextStyle,
+        ),
+        Text(
+          ' - ' + S.of(context).ClientServer + ' (with REST API);',
+          style: kTextStyle,
+        ),
+        Text(
+          ' - Java (Android Studio);',
+          style: kTextStyle,
+        ),
+        Text(
+          ' - SQL;',
+          style: kTextStyle,
+        ),
+        Text(
+          ' - NoSQL (MongoDB, Hive);',
+          style: kTextStyle,
+        ),
+        Text(
+          ' - MVP, MVC, MVVM;',
+          style: kTextStyle,
+        ),
+        Text(
+          ' - ' + S.of(context).EnglishLevel,
+          style: kTextStyle,
+        ),
+        Text(
+          ' - SOLID;',
+          style: kTextStyle,
+        ),
+        Text(
+          ' - YAGNI;',
+          style: kTextStyle,
+        ),
+        Text(
+          ' - DRY;',
+          style: kTextStyle,
         ),
       ],
     );
